7d9c75a629b76e3769d5df239a8d3af6bece66c5
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2022-11-17 08:44:05 +0100

    Ajout d'une classe FireManager

---
+++ src/main/java/Model/FireManager.java
@@ -0,0 +1,46 @@
+package Model;
+
+import Util.Position;
+import View.Grid;
+import View.PaintingVisitor;
+
+import java.util.*;
+
+public class FireManager {
+
+    Model model;
+    Set<Position> fires = new HashSet<>();
+
+    public FireManager (Model model) {
+        this.model = model;
+    }
+
+    public void extinguish(Position position) {
+        fires.remove(position);
+        model.paintingVisitor.visitEmptyBox(new EmptyBox(new Position(position.row(), position.col())));
+    }
+
+    public Position aStepTowardFire(Position position) {
+        Queue<Position> toVisit = new LinkedList<>();
+        Set<Position> seen = new HashSet<>();
+        HashMap<Position, Position> firstMove = new HashMap<>();
+        toVisit.addAll(position.next(position, model.rowCount, model.colCount));
+
+        for (Position initialMove : toVisit)
+            firstMove.put(initialMove, initialMove);
+
+        while (!toVisit.isEmpty()) {
+            Position current = toVisit.poll();
+
+            if (fires.contains(current)) return firstMove.get(current);
+
+            for (Position adjacent : position.next(current, model.rowCount, model.colCount)) {
+                if (seen.contains(adjacent)) continue;
+                toVisit.add(adjacent);
+                seen.add(adjacent);
+                firstMove.put(adjacent, firstMove.get(current));
+            }
+        }
+        return position;
+    }
+}
--- src/main/java/Model/Model.java
+++ src/main/java/Model/Model.java
@@ -7,15 +7,16 @@ import java.util.*;
 public class Model {
 
     Grid grid;
+    FireManager fireManager;
     int colCount, rowCount;
     List<Position> firefighters = new ArrayList<>();
-    Set<Position> fires = new HashSet<>();
     List<Position> ffNewPositions;
     Visitor paintingVisitor;
     int step = 0;
 
     public Model(Grid grid) {
         this.grid = grid;
+        this.fireManager = new FireManager(this);
         colCount = grid.getColCount();
         rowCount = grid.getRowCount();
         paintingVisitor = new PaintingVisitor(grid);
@@ -23,7 +24,7 @@ public class Model {
 
     public void initialisation(int fireNumber, int fireFighterNumber) {
         for (int index = 0; index < fireNumber; index++)
-            fires.add(randomPosition());
+            fireManager.fires.add(randomPosition());
         for (int index = 0; index < fireFighterNumber; index++)
             firefighters.add(randomPosition());
     }
@@ -43,15 +44,14 @@ public class Model {
         firefighters = ffNewPositions;
         if (step % 2 == 0) {
             List<Position> newFires = new ArrayList<>();
-            for (Position fire : fires) {
+            for (Position fire : fireManager.fires) {
                 newFires.addAll(activateFire(fire));
             }
             for (Position newFire : newFires)
                 paintingVisitor.visitFire(new Fire(new Position(newFire.row(), newFire.col())));
-            fires.addAll(newFires);
+            fireManager.fires.addAll(newFires);
         }
         step++;
-
     }
 
     private List<Position> activateFire(Position position) {
@@ -59,51 +59,12 @@ public class Model {
     }
 
     private Position activateFirefighter(Position position) {
-        Position randomPosition = aStepTowardFire(position);
+        Position randomPosition = fireManager.aStepTowardFire(position);
         //next(position).get((int) (Math.random()*next(position).size()));
-        List<Position> nextFires = position.next(randomPosition, rowCount, colCount).stream().filter(fires::contains).toList();
-        extinguish(randomPosition);
+        List<Position> nextFires = position.next(randomPosition, rowCount, colCount).stream().filter(fireManager.fires::contains).toList();
+        fireManager.extinguish(randomPosition);
         for (Position fire : nextFires)
-            extinguish(fire);
+            fireManager.extinguish(fire);
         return randomPosition;
-    }
-
-    private void extinguish(Position position) {
-        fires.remove(position);
-        paintingVisitor.visitEmptyBox(new EmptyBox(new Position(position.row(), position.col())));
-    }
-
-    /*
-    private List<Position> next(Position position) {
-        List<Position> list = new ArrayList<>();
-        if (position.row() > 0) list.add(new Position(position.row() - 1, position.col()));
-        if (position.col() > 0) list.add(new Position(position.row(), position.col() - 1));
-        if (position.row() < rowCount - 1) list.add(new Position(position.row() + 1, position.col()));
-        if (position.col() < colCount - 1) list.add(new Position(position.row(), position.col() + 1));
-        return list;
-    }*/
-
-    private Position aStepTowardFire(Position position) {
-        Queue<Position> toVisit = new LinkedList<>();
-        Set<Position> seen = new HashSet<>();
-        HashMap<Position, Position> firstMove = new HashMap<>();
-        toVisit.addAll(position.next(position, rowCount, colCount));
-
-        for (Position initialMove : toVisit)
-            firstMove.put(initialMove, initialMove);
-
-        while (!toVisit.isEmpty()) {
-            Position current = toVisit.poll();
-
-            if (fires.contains(current)) return firstMove.get(current);
-
-            for (Position adjacent : position.next(current, rowCount, colCount)) {
-                if (seen.contains(adjacent)) continue;
-                toVisit.add(adjacent);
-                seen.add(adjacent);
-                firstMove.put(adjacent, firstMove.get(current));
-            }
-        }
-        return position;
     }
 }
